164f60d997a0ef5280a9595fb2eb9e929fa72ad6
claudiopatti <claudiopatti1997@gmail.com>
2024-10-16 19:02:17 +0200

    primo pezzo header fatto

---
+++ config/titlesHeader.php
@@ -0,0 +1,54 @@
+<?php
+
+return [
+    [
+        "title"=> 'CHARACTERS',
+        'url' => '#',
+        'active' => false,
+    ],
+    [
+        'title'=> 'COMICS',
+        'url' => '#',
+        'active' => true,
+    ],
+    [
+        'title'=> 'MOVIES',
+        'url' => '#',
+        'active' => false,
+    ],
+    [
+        'title'=> 'TV',
+        'url' => '#',
+        'active' => false,
+    ],
+    [
+        'title'=> 'GAMES',
+        'url' => '#',
+        'active' => false,
+    ],
+    [
+        'title'=> 'COLLECTIBLES',
+        'url' => '#',
+        'active' => false,
+    ],
+    [
+        'title'=> 'VIDEOS',
+        'url' => '#',
+        'active' => false,
+    ],
+    [
+        'title'=> 'FANS',
+        'url' => '#',
+        'active' => false,
+    ],
+    [
+        'title'=> 'NEWS',
+        'url' => '#',
+        'active' => false,
+    ],
+    [
+        'title'=> 'SHOP',
+        'url' => '#',
+        'active' => false,
+    ],
+];
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $comics = config('comics');
+
+    $titlesHeader = config('titlesHeader');
+
+    return view('welcome', compact('comics', 'titlesHeader'));
 });
